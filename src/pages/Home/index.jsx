6b21d05fecbde6e3417134eeb7b843143ff32bac
Matheus <matheus-rodrigues37@live.com>
2023-01-23 16:42:43 -0300

    up

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,10 @@
 import { Container } from "./styles";
-
+import cookiesimg from "/cookies.svg";
 const Home = () => {
   return (
     <Container>
       <h1>Home</h1>
+      <img alt="cookies" src={cookiesimg} />
     </Container>
   );
 };
