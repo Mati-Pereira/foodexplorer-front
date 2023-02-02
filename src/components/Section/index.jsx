308fbd483e0c1b972af9d28f52bbf61e31f97b2e
Matheus <matheus-rodrigues37@live.com>
2023-02-02 17:18:38 -0300

    finishing home page styling

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -2,15 +2,15 @@ import { Container } from "./styles";
 import PropTypes from "prop-types";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+
 const responsive = {
   superLargeDesktop: {
-    // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
     items: 5,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
+    items: 4,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
@@ -25,7 +25,12 @@ const Section = ({ title, children }) => {
   return (
     <Container>
       <h2>{title}</h2>
-      <Carousel responsive={responsive}>{children}</Carousel>
+      <Carousel
+        responsive={responsive}
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+      >
+        {children}
+      </Carousel>
     </Container>
   );
 };
