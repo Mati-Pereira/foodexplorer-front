43a4a8855b257097b67e2b6bc6cc2461c9c11caa
Matheus <matheus-rodrigues37@live.com>
2023-02-03 16:10:22 -0300

    done with component tag style

--- src/components/Tag/styles.jsx
+++ src/components/Tag/styles.jsx
@@ -2,7 +2,10 @@ import styled from "styled-components";
 
 export const Container = styled.span`
   display: inline-block;
-  color: white;
-  background-color: red;
-  padding: 2rem;
+  color: ${({ theme }) => theme.colors.gray_300};
+  background-color: ${({ theme }) => theme.colors.gray_700};
+  padding: 0.25rem 0.5rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+  font-family: "Poppins", sans-serif;
 `;
--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -6,6 +6,7 @@ export const theme = {
     green_900: "#000A0F",
     gray_300: "#E1E1E6",
     gray_500: "#C4C4CC",
+    gray_700: "#192227",
     placeholder: "#7C7C8A",
     red: "#92000E",
     input: "#0D1D25",
