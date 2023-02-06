b91c5116a65b201e743d808ae4b34d1145f0db9a
Matheus <matheus-rodrigues37@live.com>
2023-02-06 11:34:52 -0300

    update src/pages/AddProduct/index.jsx and styles.jsx

--- src/pages/AddProduct/index.jsx
+++ src/pages/AddProduct/index.jsx
@@ -4,7 +4,7 @@ import Footer from "../../components/Footer";
 import Input from "../../components/Input";
 import DetailsAnchor from "../../components/DetailsAnchor";
 import FileInput from "../../components/FileInput";
-import Selectinput from "../../components/SelectInput";
+import SelectInput from "../../components/SelectInput";
 
 const AddProduct = () => {
   return (
@@ -21,7 +21,7 @@ const AddProduct = () => {
             id="nome"
             type="text"
           />
-          <Selectinput />
+          <SelectInput />
         </FirstRow>
       </Content>
       <Footer />
