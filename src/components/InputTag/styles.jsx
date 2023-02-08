586672734a89f6108fce2b91f7619dbec0b3df0c
Matheus <matheus-rodrigues37@live.com>
2023-02-08 15:25:19 -0300

    feat: create styles.jsx

---
+++ src/components/InputTag/styles.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  border: ${({ theme }) => `1px dashed ${theme.colors.gray_500}`};
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  gap: 0.5rem;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  > input {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    font-size: 1rem;
+    color: ${({ theme }) => theme.colors.white};
+    width: 6.25rem;
+  }
+`;
