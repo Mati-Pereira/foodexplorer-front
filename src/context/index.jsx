d2103d5aac9266b8411c4f44671692966d71c5ee
Matheus <matheus-rodrigues37@live.com>
2023-02-08 16:55:46 -0300

    update src/context/index.jsx and src/pages/Home/index.jsx

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 
 const AppContext = createContext({});
 
+const initialState = {
+  counter: 0,
+};
+
 function reducer(state, action) {
   switch (action.type) {
     case "INCREMENT":
@@ -10,14 +14,16 @@ function reducer(state, action) {
         ...state,
         counter: state.counter + 1,
       };
+    case "DECREMENT":
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
   }
 }
 
 export default function AppProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const initialState = {
-    counter: 0,
-  };
 
   return (
     <AppContext.Provider
