2facff87c509db9b3bdc830cfcdfceab49e34c65
Matheus <matheus-rodrigues37@live.com>
2023-02-11 15:09:38 -0300

    build(deps): update package-lock.json, package.json and store.js

--- src/context/app/store.js
+++ src/context/app/store.js
@@ -1,8 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth.thunk";
+import sessionStorage from "redux-persist/es/storage/session";
+import { combineReducers } from "@reduxjs/toolkit";
+import persistReducer from "redux-persist/es/persistReducer";
+
+const persistConfig = {
+  storage: sessionStorage,
+  key: "auth",
+  whitelist: ["auth"],
+};
+
+const reducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
   },
 });
