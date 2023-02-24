242639cccad0391692f9141e3cd3809793532754
Matheus <matheus-rodrigues37@live.com>
2023-02-24 17:36:31 -0300

    update src/pages/Orders/index.jsx and styles.jsx

---
+++ src/pages/Orders/index.jsx
@@ -0,0 +1,15 @@
+import { Container, Content } from "./styles";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+const Orders = () => {
+  return (
+    <Container>
+      <Header />
+      <Content></Content>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Orders;
