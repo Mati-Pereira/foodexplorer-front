ed3749b4bbbf91e74a6116095211c2321fe38fef
Matheus <matheus-rodrigues37@live.com>
2023-03-05 18:09:49 -0300

    update src/pages/AddProduct/index.jsx and src/pages/EditProduct/index.jsx

--- src/pages/EditProduct/index.jsx
+++ src/pages/EditProduct/index.jsx
@@ -107,7 +107,6 @@ const AddProduct = () => {
       .put(`/products/${id}`, fileUpload, {
         headers: {
           "Content-Type": "multipart/form-data",
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
         },
       })
       .then(() => {
