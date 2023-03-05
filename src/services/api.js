1a7ba92ff761d5713eb9a5601bff3da486e974fd
Matheus <matheus-rodrigues37@live.com>
2023-03-05 12:21:30 -0300

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "https://front-explorer-back.onrender.com/",
+  baseURL: "https://busy-teal-nematode-wig.cyclic.app/",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("access_token")}`,
   },
