813b0695447ec25197bc27739c5dfcc62ac56113
Matheus <matheus-rodrigues37@live.com>
2023-03-11 17:33:44 -0300

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
   baseURL: "https://front-explorer-back.onrender.com",
-  // baseURL: "http://localhost:3000",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("access_token")}`,
   },
