d03b22642a17e2532d4639f4a09cfdaf0aa53eac
Matheus <matheus-rodrigues37@live.com>
2023-03-12 16:26:45 -0300

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
   baseURL: "https://foodexplorer-back.onrender.com",
-
   headers: {
     Authorization: `Bearer ${localStorage.getItem("access_token")}`,
   },
