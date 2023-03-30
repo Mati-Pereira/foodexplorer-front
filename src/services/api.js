70c7be4008197006e600e0dd8e11fd72653ede7b
Matheus <matheus-rodrigues37@live.com>
2023-03-30 17:25:28 -0300

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "https://foodexplorer-back.onrender.com",
+  baseURL: "https://foodexplorer-back-6ofi7g2c0-mati-pereira.vercel.app/",
   // baseURL: "http://localhost:3000",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("access_token")}`,
