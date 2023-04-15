e79fe866529d13530454eb42e38dfae97ea5d49d
Matheus <matheus-rodrigues37@live.com>
2023-04-15 17:25:46 -0300

    build(deps): update package-lock.json and api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 export const api = axios.create({
+<<<<<<< HEAD
   baseURL: "https://foodexplorer-back-6ofi7g2c0-mati-pereira.vercel.app/",
   // baseURL: "http://localhost:3000",
+=======
+  // baseURL: "https://foodexplorer-back.onrender.com",
+  baseURL: "http://localhost:3000",
+>>>>>>> 4e6db1e (build(deps): update package-lock.json and api.js)
   headers: {
     Authorization: `Bearer ${localStorage.getItem("access_token")}`,
   },
